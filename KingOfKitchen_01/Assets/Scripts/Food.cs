03d68858527d7250201dda10fb8984f912fea353
memoriE <maxsomonkey@gmail.com>
2018-11-13 02:02:47 +0700

    update generate food feature

---
+++ KingOfKitchen_01/Assets/Scripts/Food.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Food : MonoBehaviour {
+    //Setup Food
+    public int score;
+    
+    public void DestroyGameObject()
+    {
+        Destroy(gameObject);
+    }
+
+    public void DestroyGameObjectDelay(int delay)
+    {
+        //Destroy(gameObject, delay);
+        print("Destroy already!!!");
+    }
+
+}
--- KingOfKitchen_01/Assets/Scripts/GenerateFood.cs
+++ KingOfKitchen_01/Assets/Scripts/GenerateFood.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class findLocation : MonoBehaviour {
+public class GenerateFood : MonoBehaviour {
     public GameObject locationSet;
     public GameObject foodSet;
-	void Start () {
-        
+
+    // Use this for initialization
+    void Start () {
         Transform[] locationList = locationSet.GetComponentsInChildren<Transform>();
         Transform[] foodList = foodSet.GetComponentsInChildren<Transform>();
         int locationNum = locationList.Length;
@@ -22,13 +23,11 @@ public class findLocation : MonoBehaviour {
             }
 
             int randomNum = Random.Range(1, foodNum);
-            Transform temp = Instantiate(foodList[randomNum].transform, t.position, Quaternion.identity);
-            Food food = temp.GetComponent<Food>();
-            //food.DestroyGameObjectDelay(3);
+            var temp = Instantiate(foodList[randomNum].transform, t.position, Quaternion.identity);
 
+            Destroy(temp, 5);
 
         }
-        
-	}
+    }
 	
 }
