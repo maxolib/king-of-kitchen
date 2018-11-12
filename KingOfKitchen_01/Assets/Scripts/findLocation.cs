813aa6e5d241ebe6ec860dc1e0426f6e3994833b
memoriE <maxsomonkey@gmail.com>
2018-11-12 23:54:59 +0700

    add random object feature

--- KingOfKitchen_01/Assets/Scripts/findLocation.cs
+++ KingOfKitchen_01/Assets/Scripts/findLocation.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class findLocation : MonoBehaviour {
-
-	// Use this for initialization
+    public GameObject locationSet;
+    public GameObject foodSet;
 	void Start () {
-        print("" + transform.GetChildCount());
+        
+        Transform[] locationList = locationSet.GetComponentsInChildren<Transform>();
+        Transform[] foodList = foodSet.GetComponentsInChildren<Transform>();
+        int locationNum = locationList.Length;
+        int foodNum = foodList.Length;
+        Random r = new Random();
+        
+        //show all transform
+        foreach (Transform t in locationList)
+        {
+            //int randomNum = Random.Range(0, foodNum);
+            //Instantiate(t, foodList[randomNum]);
+            print("" + locationNum);
+        }
+
+
+
+
 	}
 	
 }
