e999edece2687fd925a65b3ee46f93e36a203843
memoriE <maxsomonkey@gmail.com>
2018-11-12 15:19:08 +0700

    set test environment

---
+++ KingOfKitchen_01/Assets/Scripts/findLocation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class findLocation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        print("" + transform.GetChildCount());
+	}
+	
+}
