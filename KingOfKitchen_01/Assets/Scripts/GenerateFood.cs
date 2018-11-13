057d1bac19b99a26b5f989066f66cb44d4ef25df
memoriE <maxsomonkey@gmail.com>
2018-11-13 17:59:01 +0700

    update food feature

--- KingOfKitchen_01/Assets/Scripts/GenerateFood.cs
+++ KingOfKitchen_01/Assets/Scripts/GenerateFood.cs
@@ -5,15 +5,22 @@ using UnityEngine;
 public class GenerateFood : MonoBehaviour {
     public GameObject locationSet;
     public GameObject foodSet;
+    public float ratio;
 
     // Use this for initialization
     void Start () {
-        Transform[] locationList = locationSet.GetComponentsInChildren<Transform>();
-        Transform[] foodList = foodSet.GetComponentsInChildren<Transform>();
-        int locationNum = locationList.Length;
-        int foodNum = foodList.Length;
+        // get all objects into Arrays
+        Transform[] locationList_tmp = locationSet.GetComponentsInChildren<Transform>();
+        Transform[] foodList_tmp = foodSet.GetComponentsInChildren<Transform>();
+        // convert Array to List
+        List<Transform> locationList = new List<Transform>(locationList_tmp);
+        List<Transform> foodList = new List<Transform>(foodList_tmp);
 
-        print("" + foodNum);
+        int locationNum = locationList.Count;
+        int foodNum = foodList.Count;
+        int setNum = CalculateSetNumber(locationNum, ratio);
+        
+        
         //show all transform
         foreach (Transform t in locationList)
         {
@@ -25,9 +32,22 @@ public class GenerateFood : MonoBehaviour {
             int randomNum = Random.Range(1, foodNum);
             var temp = Instantiate(foodList[randomNum].transform, t.position, Quaternion.identity);
 
-            Destroy(temp, 5);
+            //Destroy(temp, 5);
 
         }
     }
-	
+
+    int CalculateSetNumber(int total, float ratio)
+    {
+        int result = (int)(total * ratio);
+        return result;
+    }
+
+    List<Transform> RandomPosition(List<Transform> list, int count)
+    {
+        
+        return null;
+    }
+    
+
 }
